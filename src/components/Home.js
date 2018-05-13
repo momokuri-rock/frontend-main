56aff174b2c261a68d5fd48744eb152cb7121105
rerost <gmailii@yahoo.co.jp>
2018-05-14 00:45:12 +0900

    Use scss

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
-import s from './Home.css';
-console.log(s)
+import s from './Home.scss';
 
 class Home extends Component {
   render(){
