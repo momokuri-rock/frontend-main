48e1de097d71e9f3cac723f8db290a10ff2d75cf
freewi11s <freewi11s@momokuri-rock.com>
2018-05-13 15:39:26 +0900

    demo

--- src/App.js
+++ src/App.js
@@ -4,12 +4,71 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 const App = () => (
   <BrowserRouter>
     <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/issues'>Issues</Link></li>
+      </ul>
+
+      <hr />
+
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
       <Route path='/issues' component={Issues} />
     </div>
   </BrowserRouter>
 );
+
+const IssuesData = [
+  {
+    'id': '1',
+    'name': '1',
+    'nameAAA': 'Love Challenge!',
+    'date': '20150705T000001+0900',
+    'event': '落とし神 Fall in Love FLAG 4.0',
+    'specSize': 'B5',
+    'specPage': 'モノクロ30頁',
+    'specPress': '50',
+    'specPressAdd': '20',
+    'specPrice': '500',
+    'class': '0',
+    'theme': '猪突猛進結桂本',
+    'remarks': ''
+
+  },
+  {
+    'id': '2',
+    'name': '2',
+    'nameAAA': '健全',
+    'date': '20150705T000002+0900',
+    'event': '落とし神 Fall in Love FLAG 4.0',
+    'specSize': '160cm × 50cm',
+    'specPage': '',
+    'specPress': '20',
+    'specPressAdd': '',
+    'specPrice': '3500',
+    'class': '0',
+    'theme': '五位堂結 抱き枕カバー',
+    'remarks': ''
+  },
+  {
+    'id': '3',
+    'name': '3',
+    'nameAAA': 'My Sweet Angels',
+    'date': '20160504T000001+0900',
+    'event': '落とし神 Fall in Love FLAG 4.0',
+    'specSize': 'A4',
+    'specPage': 'フルカラ24頁',
+    'specPress': '50',
+    'specPressAdd': '20',
+    'specPrice': '800',
+    'class': '0',
+    'theme': 'けいおん中心よろずイラスト本',
+    'remarks': '「PULP FICTION STORE」名義'
+  }
+];
+
+const issueByName = name => IssuesData.find( issue => issue.name === name );
 
 const Home = () => (
   <div>
@@ -26,9 +85,46 @@ const About = () => (
 const Issues = () => (
   <div>
     <h2>Issues</h2>
-    <p>HYOUSHI SAGI</p>
+    <Route exact path='/issues' component={IssueList} />
+    <Route path='/issues/:name' component={Issue} />
   </div>
 );
+
+const IssueList = () => (
+  <div>
+    {IssuesData.map(issue =>(
+      <li key={issue.name}>
+        <Link to={`/issues/${issue.name}`}>{issue.name}</Link>
+      </li>
+    ))}
+  </div>
+);
+
+const Issue = props => {
+  const { name } = props.match.params;
+  const issue = issueByName(name);
+
+  if( typeof issue === 'undefined' ){
+    return (
+      <div>
+        <p>Issues with name '{name}' is noe exist.</p>
+      </div>
+    );
+  }
+
+  const containStyle = { 'border':'1px gray solid', 'padding':'10px' }
+
+  return (
+    <div>
+      <div style={containStyle}>
+        <p>{issue.name}</p>
+        <p>{issue.event}</p>
+        <p>{issue.specSize} / {issue.specPage}</p>
+        <p>{issue.theme}</p>
+      </div>
+    </div>
+  );
+}
 
 export default App;
 /*
@@ -39,12 +135,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React123</h1>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React123</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
