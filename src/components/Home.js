9116ca5aac83732f58aa01ab44d2475e896fb360
freewi11s <freewi11s@momokuri-rock.com>
2018-05-17 23:54:22 +0900

    replace scss /components > /styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import s from './Home.scss';
+import s from '../styles/Home.scss';
 
 class Home extends Component {
   render(){
     return(
       <div className={s.main}>
-        <h2>Home</h2>
+        <h2 className={s.main__title}>Home</h2>
         <p>Welcome to Momokuri-Rock</p>
       </div>
     );
